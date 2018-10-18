76b104013aa22c0c13576998a56a9bb7bc3c70f5
User <User@DESKTOP-B2GO7R3>
2018-10-18 03:08:12 -0400

    Add project files.

---
+++ MotencDemo/MainWindow.xaml.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace MotencDemo
+{
+    public partial class MainWindow : Window
+    {
+        private MotencAPI motencAPI;
+
+        private Semaphore sem = new Semaphore(1, 1);
+        private Thread readThread;
+        private ManualResetEvent cancelReadThread = new ManualResetEvent(false);
+
+        private SolidColorBrush defaultLEDColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF4F4F5"));
+
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+            createRegCboBoxes();
+            motencAPI = new MotencAPI();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateUI();
+            readThread = new Thread(new ThreadStart(ReadThread));
+            readThread.IsBackground = true;
+            readThread.Start();
+        }
+
+        private void wndwMain_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Environment.Exit(0);
+            motencAPI.Dispose();
+        }
+
+        private delegate void UpdateUIDelegate();
+        private void UpdateUI()
+        {
+            if(!Dispatcher.CheckAccess())
+            {
+                Dispatcher.BeginInvoke(new UpdateUIDelegate(UpdateUI));
+                return;
+            }
+
+            lock(sem)
+            {
+                txtEnc1.Text = Convert.ToString(motencAPI.ReadEncoder(0));
+                txtEnc2.Text = Convert.ToString(motencAPI.ReadEncoder(1));
+                txtEnc3.Text = Convert.ToString(motencAPI.ReadEncoder(2));
+                txtEnc4.Text = Convert.ToString(motencAPI.ReadEncoder(3));
+
+                intializeLEDs(grdInputs, true);
+                intializeLEDs(grdOutputs, false);
+
+                txtReg0.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs1.SelectedIndex));
+                txtReg1.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs2.SelectedIndex));
+                txtReg2.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs3.SelectedIndex));
+                txtReg3.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs4.SelectedIndex));
+                txtReg4.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs5.SelectedIndex));
+                txtReg5.Text = String.Format("{0:X8}", motencAPI.ReadRegister(cboRegs6.SelectedIndex));
+
+                txtReadADC0.Text = String.Format("{0}", motencAPI.ReadAnalogIN(cboADCBank.SelectedIndex, 0));
+                txtReadADC1.Text = String.Format("{0}", motencAPI.ReadAnalogIN(cboADCBank.SelectedIndex, 1));
+                txtReadADC2.Text = String.Format("{0}", motencAPI.ReadAnalogIN(cboADCBank.SelectedIndex, 2));
+                txtReadADC3.Text = String.Format("{0}", motencAPI.ReadAnalogIN(cboADCBank.SelectedIndex, 3));
+            }
+        }
+
+        private void createRegCboBoxes()
+        {
+            List<int> regRange = new List<int>();
+            for (int i = 0; i < 41; i++)
+                regRange.Add(i);
+            cboRegs1.ItemsSource = regRange;
+            cboRegs2.ItemsSource = regRange;
+            cboRegs3.ItemsSource = regRange;
+            cboRegs4.ItemsSource = regRange;
+            cboRegs5.ItemsSource = regRange;
+            cboRegs6.ItemsSource = regRange;
+        }
+
+        private void ReadThread()
+        {
+            while (true)
+            {
+                if (cancelReadThread.WaitOne(100, false))
+                    return;
+
+                UpdateUI();
+            }
+        }
+
+        private void txtEnc1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBoxResult rs = MessageBox.Show("Reset Encoder 0?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            lock(sem)
+                motencAPI.ResetEncoder(0);
+        }
+
+        private void txtEnc2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBoxResult rs = MessageBox.Show("Reset Encoder 1?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            lock (sem)
+                motencAPI.ResetEncoder(1);
+        }
+
+        private void txtEnc3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBoxResult rs = MessageBox.Show("Reset Encoder 2?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            lock (sem)
+                motencAPI.ResetEncoder(2);
+        }
+
+        private void txtEnc4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBoxResult rs = MessageBox.Show("Reset Encoder 3?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            lock (sem)
+                motencAPI.ResetEncoder(3);
+        }
+
+        private void btnRstAllEncs_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(this);
+            MessageBoxResult rs = MessageBox.Show("Reset All Encoder Values?", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            lock (sem)
+                for (int enc = 0; enc < 4; enc++)
+                    motencAPI.ResetEncoder(enc);
+        }
+
+        private void intializeLEDs(Grid grid, bool input)
+        {
+            int index = 0;
+            foreach (StackPanel sp in grid.Children)
+                foreach(var el in sp.Children)
+                    if(el is Ellipse)
+                        changeLEDColor((Ellipse)el, index++, input);
+        }
+
+        private void changeLEDColor(Ellipse led, int index, bool input)
+        {
+            long ioData = input ? motencAPI.DigitalInputs : motencAPI.DigitalOutputs;
+            if (input)
+                led.Fill = (ioData & (1 << index)) == 0 ? led.Fill = new SolidColorBrush(Colors.GreenYellow) : defaultLEDColor;
+            else
+                led.Fill = (ioData & (1 << index)) == 0 ? led.Fill = defaultLEDColor : new SolidColorBrush(Colors.GreenYellow);
+        }
+
+        private void setOutput(object sender)
+        {
+            Ellipse outputLED = (Ellipse)sender;
+            int index = int.Parse(new string(outputLED.Name.Where(char.IsDigit).ToArray()));
+
+            motencAPI.DigitalOutputs ^= (ushort)(1 << index);
+            changeLEDColor(outputLED, index, false);
+        }
+
+        private void ledOut0_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut5_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut6_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut7_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut8_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut9_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut10_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut11_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut12_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut13_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut14_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void ledOut15_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            setOutput(sender);
+        }
+
+        private void btnDACPlus_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int channel = cboDACChannels.SelectedIndex;
+            motencAPI.SetDAC(channel, 10);
+        }
+
+        private void btnDACPlus_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            int channel = cboDACChannels.SelectedIndex;
+            motencAPI.SetDAC(channel, 0);
+        }
+
+        private void btnDACNeg_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int channel = cboDACChannels.SelectedIndex;
+            motencAPI.SetDAC(channel, -10);
+        }
+
+        private void btnDACNeg_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            int channel = cboDACChannels.SelectedIndex;
+            motencAPI.SetDAC(channel, 0);
+        }
+    }
+}
---
+++ MotencDemo/MotencAPI.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace MotencDemo
+{
+    public class MotencAPI
+    {
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalInit();
+
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalQuit();
+
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalReadReg(int reg, out int regData);
+
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalWriteReg(int reg, int regData);
+
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalDacWrite(int axis, double volts);
+
+        [DllImport(@"WINMOTENC.dll")]
+        public static extern int vitalReadAnalogInputs(int nBank, IntPtr values);
+
+        private enum PlxStatusCode
+        {
+            PLX_STATUS_OK = 512,
+            PLX_STATUS_FAILED = 513,
+            PLX_STATUS_NULL_PARAM = 514,
+            PLX_STATUS_UNSUPPORTED = 515,
+            PLX_STATUS_NO_DRIVER = 516,
+            PLX_STATUS_INVALID_OBJECT = 517,
+            PLX_STATUS_VER_MISMATCH = 518,
+            PLX_STATUS_INVALID_OFFSET = 519,
+            PLX_STATUS_INVALID_DATA = 520,
+            PLX_STATUS_INVALID_SIZE = 521,
+            PLX_STATUS_INVALID_ADDR = 522,
+            PLX_STATUS_INVALID_ACCESS = 523,
+            PLX_STATUS_INSUFFICIENT_RES = 524,
+            PLX_STATUS_TIMEOUT = 525,
+            PLX_STATUS_CANCELED = 526,
+            PLX_STATUS_COMPLETE = 527,
+            PLX_STATUS_PAUSED = 528,
+            PLX_STATUS_IN_PROGRESS = 529,
+            PLX_STATUS_PAGE_GET_ERROR = 530,
+            PLX_STATUS_PAGE_LOCK_ERROR = 531,
+            PLX_STATUS_LOW_POWER = 532,
+            PLX_STATUS_IN_USE = 533,
+            PLX_STATUS_DISABLED = 534,
+            PLX_STATUS_PENDING = 535,
+            PLX_STATUS_NOT_FOUND = 536,
+            PLX_STATUS_INVALID_STATE = 537,
+            PLX_STATUS_BUFF_TOO_SMALL = 538,
+            PLX_STATUS_RSVD_LAST_ERROR = 539
+        }
+
+        private void reportError(PlxStatusCode errorCode)
+        {
+            string output = "";
+            switch(errorCode)
+            {
+                case PlxStatusCode.PLX_STATUS_FAILED:
+                    output += "PLX_STATUS_FAILED";
+                    break;
+                case PlxStatusCode.PLX_STATUS_NULL_PARAM:
+                    output += "PLX_STATUS_NULL_PARAM";
+                    break;
+                case PlxStatusCode.PLX_STATUS_UNSUPPORTED:
+                    output += "PLX_STATUS_UNSUPPORTED";
+                    break;
+                case PlxStatusCode.PLX_STATUS_NO_DRIVER:
+                    output += "PLX_STATUS_NO_DRIVER";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_OBJECT:
+                    output += "PLX_STATUS_INVALID_OBJECT";
+                    break;
+                case PlxStatusCode.PLX_STATUS_VER_MISMATCH:
+                    output += "PLX_STATUS_VER_MISMATCH";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_OFFSET:
+                    output += "PLX_STATUS_INVALID_OFFSET";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_DATA:
+                    output += "PLX_STATUS_INVALID_DATA";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_SIZE:
+                    output += "PLX_STATUS_INVALID_SIZE";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_ADDR:
+                    output += "PLX_STATUS_INVALID_ADDR";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_ACCESS:
+                    output += "PLX_STATUS_INVALID_ACCESS";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INSUFFICIENT_RES:
+                    output += "PLX_STATUS_INSUFFICIENT_RES";
+                    break;
+                case PlxStatusCode.PLX_STATUS_TIMEOUT:
+                    output += "PLX_STATUS_TIMEOUT";
+                    break;
+                case PlxStatusCode.PLX_STATUS_CANCELED:
+                    output += "PLX_STATUS_CANCELED";
+                    break;
+                case PlxStatusCode.PLX_STATUS_COMPLETE:
+                    output += "PLX_STATUS_COMPLETE";
+                    break;
+                case PlxStatusCode.PLX_STATUS_PAUSED:
+                    output += "PLX_STATUS_PAUSED";
+                    break;
+                case PlxStatusCode.PLX_STATUS_IN_PROGRESS:
+                    output += "PLX_STATUS_IN_PROGRESS";
+                    break;
+                case PlxStatusCode.PLX_STATUS_PAGE_GET_ERROR:
+                    output += "PLX_STATUS_PAGE_GET_ERROR";
+                    break;
+                case PlxStatusCode.PLX_STATUS_PAGE_LOCK_ERROR:
+                    output += "PLX_STATUS_PAGE_LOCK_ERROR";
+                    break;
+                case PlxStatusCode.PLX_STATUS_LOW_POWER:
+                    output += "PLX_STATUS_LOW_POWER";
+                    break;
+                case PlxStatusCode.PLX_STATUS_IN_USE:
+                    output += "PLX_STATUS_IN_USE";
+                    break;
+                case PlxStatusCode.PLX_STATUS_DISABLED:
+                    output += "PLX_STATUS_DISABLED";
+                    break;
+                case PlxStatusCode.PLX_STATUS_PENDING:
+                    output += "PLX_STATUS_PENDING";
+                    break;
+                case PlxStatusCode.PLX_STATUS_NOT_FOUND:
+                    output += "PLX_STATUS_NOT_FOUND";
+                    break;
+                case PlxStatusCode.PLX_STATUS_INVALID_STATE:
+                    output += "PLX_STATUS_INVALID_STATE";
+                    break;
+                case PlxStatusCode.PLX_STATUS_BUFF_TOO_SMALL:
+                    output += "PLX_STATUS_BUFF_TOO_SMALL";
+                    break;
+                case PlxStatusCode.PLX_STATUS_RSVD_LAST_ERROR:
+                    output += "PLX_STATUS_RSVD_LAST_ERROR";
+                    break;
+            }
+            MessageBoxResult rs = MessageBox.Show(output, "Confirm", MessageBoxButton.OK, MessageBoxImage.Question);
+            Environment.Exit(0);
+            this.Dispose();
+        }
+
+        public MotencAPI()
+        {
+            PlxStatusCode errorCode = (PlxStatusCode)vitalInit();
+            if (errorCode != PlxStatusCode.PLX_STATUS_OK)
+            {
+                reportError(errorCode);
+                throw new Exception("vitalInit()");
+            }
+        }
+
+        public virtual void Dispose()
+        {
+            vitalQuit();
+        }
+
+        public void SetDAC(int channel, double volts)
+        {
+            if (vitalDacWrite(channel, volts) == 0)
+                throw new Exception(string.Format("vitalWriteReg( {0} )", (24 + channel)));
+        }
+
+        public void ResetEncoder(int encoder)
+        {
+            if (encoder < 0 || encoder >= 4)
+                throw new Exception(String.Format("Invalid encoder index: {0}", encoder));
+
+            int regData = (0x00000001 << encoder);
+            if (vitalWriteReg(5, regData) == 0)
+                throw new Exception("vitalWriteReg( 5 )");
+        }
+
+        public int ReadEncoder(int encoder)
+        {
+            if (encoder < 0 || encoder >= 4)
+                throw new Exception(String.Format("Invalid encoder index: {0}", encoder));
+
+            int count = 0;
+            if (vitalReadReg(encoder, out count) == 0)
+                throw new Exception(String.Format("vitalReadReg( {0} )", encoder));
+            return count;
+        }
+
+        public double ReadAnalogIN(int nBank, int channel)
+        {
+            double[] values = {0, 0, 0, 0};
+            IntPtr ptr = Marshal.AllocCoTaskMem(sizeof(double) * values.Length);
+            if (vitalReadAnalogInputs(nBank, ptr) == 0)
+                throw new Exception("vitalReadAnalogIN( 32 )");
+            Marshal.Copy(ptr, values, 0, values.Length); 
+            Marshal.FreeHGlobal(ptr);
+            return values[channel];
+        }
+
+        public ushort DigitalOutputs
+        {
+            get
+            {
+                int regData = 0;
+                if (vitalReadReg(4, out regData) == 0)
+                    throw new Exception("vitalReadReg( 4 )");
+                return (ushort)(regData & 0x0000FFFF);
+            }
+
+            set
+            {
+                int regData = value;
+                if (vitalWriteReg(4, regData) == 0)
+                    throw new Exception("vitalWriteReg( 4 )");
+            }
+        }
+
+        public uint DigitalInputs
+        {
+            get
+            {
+                int j4Data = 0; // inputs 0..15 in bits 16..31
+                if (vitalReadReg(4, out j4Data) == 0)
+                    throw new Exception("vitalReadReg( 4 )");
+
+                int j5Data = 0; // inputs 16..31 in bits 0..15
+                if (vitalReadReg(5, out j5Data) == 0)
+                    throw new Exception("vitalReadReg( 5 )");
+
+                return (((uint)j4Data & 0xFFFF0000) >> 16) | (((uint)j5Data & 0x0000FFFF) << 16);
+            }
+
+        }
+
+        public int ReadRegister(int register)
+        {
+            int regData;
+            if (vitalReadReg(register, out regData) == 0)
+                throw new Exception(String.Format("ReadRegister( {0} )", register));
+            return regData;
+        }
+    }
+}
